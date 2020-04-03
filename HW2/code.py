e0f4f9c792a3799fb958466a800da6fbbc4091d6
andreamatt <andreamatte97@gmail.com>
2020-04-03 15:44:23 +0200

    fixed EM alg

--- HW2/code.py
+++ HW2/code.py
@@ -13,28 +13,68 @@ def variance(values):
 def std(values):
     return np.std(values, ddof=1)
 
-data = 'HW2\\data'
-data_ex1 = read_csv(f'{data}\\data_ex1.csv', header=None)
+data = 'HW2/data'
 data_ex2 = read_csv(f'{data}\\data_ex2.csv', header=None)
 
-# TODO: EXERCISE 1
-# * Load the data from the CSV file data_ex1.csv. This data represents measurements of some quantity over a few days.
-# * In each line: the 1st value refers to the time of the measurement; the 2nd value is the measurement output.
-# * If you draw a scatter plot, you should see a clear trend in the data.
-# * 1. Use least squares to remove the trend. You only need polynomial functions for this.
-# * 2. After having verified that a good value for the maximum degree of the the polynomial is 5, remove the
-# * trend from the data and fit a Gaussian distribution to the resulting dataset.
-# * 3. Give the mean and variance of the distribution, and draw a QQ-plot to determine if the Gaussian approximation holds. Give a prediction interval for future samples from this Gaussian distribution.
-# * 4. Discuss what would happen if you fit a polynomial of degree different than 5 to the data.
-values = data_ex1.to_numpy()
-# print(values)
-X = values[:,0]
-Y = values[:,1]
-a, b, c, d, e, f = np.polyfit(X, Y, 5)
-plt.scatter(X, Y, s=2)
-plt.scatter(X, a*np.power(X,5)+b*np.power(X,4)+c*np.power(X,3)+d*np.power(X,2)+e*X +f, s=2)
+# Exercise 2
+# Load the data from the CSV file data_ex2.csv. These are samples from three different, independent Gaussian
+# distributions, all mixed together.
+# 1. Implement the Expectation-Maximization algorithm to fit a mixture of three Gaussian distributions to the data.
+#    Try both with and without the prior update step. Discuss the results.
+# 2. Give the parameters of the distributions thus found, and plot the corresponding PDFs on top of the
+#    empirical PDFs of the data (e.g., the histogram).
+
+values = data_ex2.to_numpy()[:,0]
+
+from time import time
+def expectation_maximization_normal(values, curve_n, iterations, curve_prob=None, prior=True):
+    n = len(values)
+    # use the same std for all of them
+    deviations = [std(values)/curve_n]*curve_n
+    # for 3 curves, use 1/4, 2/4, 3/4 quantiles as means
+    means = [quantile(values, (i+1)/(curve_n+1)) for i in range(curve_n)]
+    if curve_prob==None:
+        curve_prob = [1/curve_n]*curve_n
+    #print([(means[i], deviations[i], curve_prob[i]) for i in range(curve_n)])
+
+    for iteration in range(iterations):
+        denoms = np.zeros(n)
+        for curve in range(curve_n):
+            denoms += norm.pdf(values, means[curve], deviations[curve])*curve_prob[curve]
+            
+        for curve in range(curve_n):
+            b = []
+            m = means[curve]
+            s = deviations[curve]
+            b = norm.pdf(values,m,s)/denoms
+            new_m = sum(b*values) / sum(b)
+            new_s = sqrt( sum(b*((values-new_m)**2)) / sum(b) )
+            means[curve] = new_m
+            deviations[curve] = new_s
+        #print([(means[i], deviations[i], curve_prob[i]) for i in range(curve_n)])
+
+    return [(means[i], deviations[i], curve_prob[i]) for i in range(curve_n)]
+    
+start = time()
+print("prior true")
+curves = expectation_maximization_normal(values, 3, 100, prior=True)
+print(curves)
+plt.hist(values, bins=50, density=True, color='y', linewidth=0.1, edgecolor='b')
+X_prob = np.arange(min(values), max(values), 1/100)
+for c in curves:
+    plt.scatter(X_prob, c[2]*norm.pdf(X_prob, c[0], c[1] ), s=1, zorder=2)
+
+
+end = time()
+print(end-start)
 plt.show()
 
-def polyfit(X, Y, degree):
-    # return X Y of fitted line
-    pass
+print("prior false")
+curves = expectation_maximization_normal(values, 3, 100, prior=False)
+print(curves)
+plt.hist(values, bins=50, density=True, color='y', linewidth=0.1, edgecolor='b')
+X_prob = np.arange(min(values), max(values), 1/100)
+for c in curves:
+    plt.scatter(X_prob, c[2]*norm.pdf(X_prob, c[0], c[1] ), s=1, zorder=2)
+plt.show()
+
